d0934b4bd75fdd83e6307bcf2aa9ccb7d2d06529
shilpavijay <shilpa.amberker@gmail.com>
2017-06-23 19:42:14 +0530

    NewFile

--- script.js
+++ script.js
@@ -3,4 +3,43 @@ var abc = new Vue({
 	data: {
 		"test": "Hello Vue!!!"
 	}
+});
+
+var firstApp = new Vue({
+  el: "#firstApp",
+data: {message: "Hello World!!!"
+      }
+});
+
+Vue.component('to-do', {
+  props: ['todo'],
+  template: '<li> {{ todo.comp }} </li>'
 })
+
+var newapp = new Vue({
+  el: '#newapp',
+  data: {
+    hovermsg: 'You loaded this page on ' + new Date(),
+    list: [
+      { items: 'Pandas' },
+      { items: 'Scikit-Learn'},
+      { items: 'Matplotlib'},
+      { items: 'BeautifulSoup'}
+    ],
+    
+    todolist: [
+      { id: 0, comp: 'Buy Abc' },
+      { id: 1, comp: 'Go to xyz' }
+    ],
+    msg: 'Vue.js is Fun!',
+    text: 'Change me!'
+  },
+  methods: {
+    doThisOnClick: function(){
+      this.msg = this.msg.split('').reverse().join('')
+    }
+  }
+});
+
+
+
