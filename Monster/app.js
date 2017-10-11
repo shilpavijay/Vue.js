d36d8ad4a2c5cb9688b086b8d1f3b455c1f5c1b7
shilpavijay <shilpa.amberker@gmail.com>
2017-10-11 10:26:14 +0530

    attackFunc

--- Monster/app.js
+++ Monster/app.js
@@ -12,6 +12,26 @@ new Vue({
 			this.monsterHealth = 100
 		},
 		attack: function() {
+			var max = 10
+			var min = 3
+			var damage = Math.max(Math.floor(Math.random() * max)+1, min)
+			this.monsterHealth -= damage;
+
+			if (this.monsterHealth <= 0){
+				alert('You Won!!!');
+				this.gameIsRunning = false;
+				return;
+			}
+
+			var max = 12;
+			var min = 5;
+			damage = Math.max(Math.floor(Math.random() * max)+1, min)
+			this.playerHealth -= damage
+
+			if (this.playerHealth <= 0){
+				alert('You lost!!!');
+				this.gameIsRunning = false;
+			}	
 
 		},
 		specialAttack: function() {
@@ -21,7 +41,7 @@ new Vue({
 
 		},
 		giveUp: function() {
-			
+
 		}		
 	}
 })
