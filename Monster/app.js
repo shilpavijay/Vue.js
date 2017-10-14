39966b6a66548f09dd4d3a39d81c68c4f4b95b87
shilpavijay <shilpa.amberker@gmail.com>
2017-10-14 19:59:22 +0530

    splAttack

--- Monster/app.js
+++ Monster/app.js
@@ -17,17 +17,26 @@ new Vue({
 			if (this.checkWin()) {
 				return;
 			}
-			this.playerHealth -= this.calculateDamage(5,12)
-			this.checkWin();
+			this.monsterAttack();
+			
 		},
 		specialAttack: function() {
+			this.monsterHealth -= this.calculateDamage(10,20)
+			if (this.checkWin()) {
+				return;
+			}
 
+			this.monsterAttack();
 		},
 		heal: function() {
 
 		},
 		giveUp: function() {
 
+		},
+		monsterAttack: function() {
+			this.playerHealth -= this.calculateDamage(5,12)
+			this.checkWin();
 		},
 		calculateDamage: function(min,max) {
 			return Math.max(Math.floor(Math.random() * max)+1, min)
