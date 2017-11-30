7e0506b524a9d13b9b6a3b85110865fbbc9b019e
shilpavijay <shilpa.amberker@gmail.com>
2017-11-30 14:57:48 +0530

    assignmentcompl

--- server-status/src/App.vue
+++ server-status/src/App.vue
@@ -3,27 +3,11 @@
         <server-header></server-header>
         <hr>
         <div class="row">
-            <div class="col-xs-12 col-sm-6">
-                <ul class="list-group">
-                    <li
-                            class="list-group-item"
-                            v-for="index in 5">
-                        Server #{{ index }}
-                    </li>
-                </ul>
-            </div>
-            <div class="col-xs-12 col-sm-6">
-                <p>Server Details are currently not updated</p>
-            </div>
-        </div>
+	        <servers></servers>
+	        <server-details></server-details>
+	    </div>
         <hr>
-        <div class="row">
-            <div class="col-xs-12">
-                <footer>
-                    <p>All Servers are managed here</p>
-                </footer>
-            </div>
-        </div>
+        <server-footer></server-footer>
     </div>
 </template>
 
--- server-status/src/main.js
+++ server-status/src/main.js
@@ -1,8 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
 import Header from './Header.vue'
+import Servers from './Servers.vue'
+import ServerDetails from './ServerDetails.vue'
+import Footer from './Footer.vue'
 
 Vue.component('server-header', Header)
+Vue.component('servers', Servers)
+Vue.component('server-details', ServerDetails)
+Vue.component('server-footer', Footer)
 
 new Vue({
   el: '#app',
