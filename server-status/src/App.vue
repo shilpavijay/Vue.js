b24f0fe8d36c3b09de5fd2c733643ed9dd6b631b
shilpavijay <shilpa.amberker@gmail.com>
2017-11-30 15:26:40 +0530

    compdiffways

--- server-status/src/App.vue
+++ server-status/src/App.vue
@@ -4,7 +4,7 @@
         <hr>
         <div class="row">
 	        <servers></servers>
-	        <server-details></server-details>
+	        <serverDetails></serverDetails>
 	    </div>
         <hr>
         <server-footer></server-footer>
@@ -12,6 +12,19 @@
 </template>
 
 <script>
+    import Header from './components/shared/Header.vue'
+    import Servers from './components/Servers.vue'
+    import ServerDetails from './components/ServerDetails.vue'
+    import Footer from './components/shared/Footer.vue'
+
+    export default {
+        components: {
+            serverHeader: Header,
+            Servers,
+            serverDetails: ServerDetails,
+            serverFooter: Footer
+        }
+    }
 </script>
 
 <style>
--- server-status/src/main.js
+++ server-status/src/main.js
@@ -1,14 +1,5 @@
 import Vue from 'vue'
 import App from './App.vue'
-import Header from './components/shared/Header.vue'
-import Servers from './components/Servers.vue'
-import ServerDetails from './components/ServerDetails.vue'
-import Footer from './components/shared/Footer.vue'
-
-Vue.component('server-header', Header)
-Vue.component('servers', Servers)
-Vue.component('server-details', ServerDetails)
-Vue.component('server-footer', Footer)
 
 new Vue({
   el: '#app',
