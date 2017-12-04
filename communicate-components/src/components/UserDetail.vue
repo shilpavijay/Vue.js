b4802a54514f54a40d0d2bc3bade41fd4603a911
shilpavijay <shilpa.amberker@gmail.com>
2017-12-04 14:03:55 +0530

    props

--- communicate-components/src/components/UserDetail.vue
+++ communicate-components/src/components/UserDetail.vue
@@ -2,10 +2,19 @@
     <div class="component">
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
+        <p>Name: {{ revName() }}</p>
     </div>
 </template>
 
 <script>
+	export default {
+		props: ['name'],
+		methods: {
+			revName() {
+				return this.name.split("").reverse("").join("");
+			}
+		}
+	}
 </script>
 
 <style scoped>
