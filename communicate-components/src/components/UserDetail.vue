928db8d70168213fa3d345d0df19223d91f55e21
shilpavijay <shilpa.amberker@gmail.com>
2017-12-04 14:46:57 +0530

    emit

--- communicate-components/src/components/UserDetail.vue
+++ communicate-components/src/components/UserDetail.vue
@@ -3,15 +3,25 @@
         <h3>You may view the User Details here</h3>
         <p>Many Details</p>
         <p>Name: {{ revName() }}</p>
+        <button @click="resetName">Reset Name</button>
     </div>
 </template>
 
 <script>
 	export default {
-		props: ['name'],
+		props: {
+			name: {
+				type: String,
+			 default: 'I am Default'
+			}
+		},
 		methods: {
 			revName() {
 				return this.name.split("").reverse("").join("");
+			},
+			resetName() {
+				this.name = "Shilpa",
+				this.$emit('nameReset',this.name)
 			}
 		}
 	}
