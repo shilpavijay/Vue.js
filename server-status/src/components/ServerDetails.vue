cf29417ede2dcb0d7e32e0b73c59cbf669e44542
shilpavijay <shilpa.amberker@gmail.com>
2017-12-05 16:33:30 +0530

    section8Conclusion

--- server-status/src/components/ServerDetails.vue
+++ server-status/src/components/ServerDetails.vue
@@ -1,13 +1,40 @@
 <template>
 	<div class="col-xs-12 col-sm-6">
-        <p>Server Details are currently not updated</p>
+		<button @click="clicked">Get Status</button>
+		<br />
+		<div v-if="stat">
+	        <p>Click on a server to view details</p>
+	        <p>ServerID: {{ id }}</p>
+	        <p>Server Status: {{ status }}</p>
+        </div>
     </div>
 </template>
 
-<script></script>
+<script>
+	import { eventBus } from '../main'
+
+	export default {
+		data: function() {
+			return {
+				id: '',
+				status: '',
+				stat: false
+			}
+		},
+		methods: {
+			clicked() {
+				this.stat = true
+				eventBus.$on('server', (data) => {
+					this.id = data.id
+					this.status = data.status
+				})
+			}
+		}
+	}
+</script>
 	
 <style scoped>
 	div {
-		border: 1px solid red;
+		/*border: 1px solid;*/
 	}
 </style>
--- server-status/src/components/Servers.vue
+++ server-status/src/components/Servers.vue
@@ -3,17 +3,42 @@
         <ul class="list-group">
             <li
                     class="list-group-item"
-                    v-for="index in 5">
-                Server #{{ index }}
+                    v-for="s in servers"
+                    @click="select(s)">
+                Server #{{ s.id }}
+                Status: {{ s.status }}
             </li>
         </ul>
     </div>
 </template>
 
-<script></script>
+<script>
+    import { eventBus } from '../main'
+
+    export default {
+        data: function() {
+            return {
+                servers: [
+                    {id: 1, status: 'Normal'},
+                    {id: 2, status: 'Critical'},
+                    {id: 3, status: 'Unknown'},
+                    {id: 4, status: 'Normal'},
+                    {id: 5, status: 'Unknown'}
+                ]
+            }
+        },
+
+        methods: {
+            select(s) {
+                // alert(s.id)
+                eventBus.$emit('server', s);
+            }
+        }
+    }
+</script>
 
 <style scoped>
 	div {
-		border: 1px solid blue;
+		/*border: 1px solid;*/
 	}
 </style>
