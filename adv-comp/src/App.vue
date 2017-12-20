d77dfe2f49918c73ea8212e0583fc195e6b47365
shilpavijay <shilpa.amberker@gmail.com>
2017-12-20 14:51:32 +0530

    MultipleSlots

--- adv-comp/src/App.vue
+++ adv-comp/src/App.vue
@@ -3,8 +3,8 @@
     <div class="row">
       <div class="col-xs-12">
         <app-quote>
-          <h2>{{ quote }}</h2>
-          <p>A wonderful Quote</p>
+          <h2 slot="title">{{ quoteTitle }}</h2>
+          <p slot="content">A wonderful Quote</p>
         </app-quote> 
       </div>
     </div>
@@ -17,9 +17,9 @@
   export default {
     data: function() {
       return {
-        quote: "The Quote"
+        quoteTitle: "The Quote"
       }
-    },
+    }, 
     components: {
       appQuote: Quote
     }
