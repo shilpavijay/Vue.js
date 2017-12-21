8df1dac8d4dd86d122e1179957333a777e502294
shilpavijay <shilpa.amberker@gmail.com>
2017-12-21 17:20:46 +0530

    appVuechanges

--- quotes/src/App.vue
+++ quotes/src/App.vue
@@ -1,12 +1,19 @@
 <template>
     <div class="container">
-    
+     {{ quotes }}
     </div>
 </template>
 
 <script>
     export default {
-        
+        data: function() {
+        	return {
+        		quotes: [
+        			'First Quote'
+        		],
+        		maxQuotes: 10
+        	}
+        }
     }
 </script>
 
